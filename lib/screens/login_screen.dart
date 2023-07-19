005156b03c9359e811b1b62f57fa6a32b0de3f51
ext_iestrada <ext_iestrada@tottus.com.pe>
2023-07-19 14:58:07 -0500

    Se agrego loading y navegar a otra pantalla

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -101,13 +101,23 @@ class _LoginForm extends StatelessWidget {
               child: Container(
                 padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
                 child: Text(
-                  'Ingresar',
+                  loginForm.isLoading ?
+                  'Espere' : 'Ingresar',
                   style: TextStyle(color: Colors.white),
                 ),
               ),
-              onPressed: (){
-                // TODO Login
+              onPressed: loginForm.isLoading ? null : () async {
+                
+                FocusScope.of(context).unfocus();
+
                 if(!loginForm.isValidForm()) return;
+
+                loginForm.isLoading = true;
+
+                await Future.delayed(Duration(seconds: 2));
+
+                // TODO Validar si el login es correcto
+                loginForm.isLoading = false;
 
                 Navigator.pushReplacementNamed(context, 'home');
               }
